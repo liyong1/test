e01a2c8c84fe369016b19956521000dff0f9fdf2
liyong1 <1191256202@qq.com>
2022-09-22 12:29:46 +0800

    clear test

--- src/com/boris/pattern/model1/OperationSub.java
+++
@@ -1,8 +0,0 @@
-package com.boris.pattern.model1;
-
-public class OperationSub extends Operation{
-    @Override
-    public Double getResult() {
-        return getNumberA() - getNumberB();
-    }
-}
